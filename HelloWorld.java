3e3a86c0858590ea76e78eb602757dd9a14a640a
adityash13 <as.aditya1309@gmail.com>
2024-09-08 23:10:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("This is Jenkins Job using Poll SCM webhook");
+		System.out.println("This is Jenkins Job using Poll SCM webhooks");
 
 	}
 
